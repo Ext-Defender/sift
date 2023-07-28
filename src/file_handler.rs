ae94e611b63f3b2a7f5e626998d2def01036365d
Bryan Vinton <bryan.vinton18@gmail.com>
2023-07-28 10:11:59 -0400

    Extracted text searching function on file handlers

--- src/file_handler.rs
+++ src/file_handler.rs
@@ -1,6 +1,5 @@
 use msg_parser::Outlook;
 use regex::Regex;
-use std::error::Error;
 use std::fs;
 use std::io::BufReader;
 use std::io::Read;
@@ -13,16 +12,12 @@ pub fn scan_file(path: &PathBuf, patterns: &Vec<Regex>) -> Option<(Vec<String>, 
     let ret = match path.extension() {
         Some(ext) => match ext.to_str() {
             Some("pdf") => scan_pdf(&path, &patterns),
-            Some("xlsx") | Some("pptx") | Some("docx") => {
-                scan_ooxml(&path, &patterns).unwrap_or(None)
-            }
+            Some("xlsx") | Some("pptx") | Some("docx") => scan_ooxml(&path, &patterns),
             Some("txt") | Some("xml") | Some("html") | Some("htm") | Some("csv") => {
-                scan_txt(&path, &patterns).unwrap_or(None)
+                scan_txt(&path, &patterns)
             }
-            Some("rtf") | Some("wpd") => scan_rtf(&path, &patterns).unwrap_or(None),
-            Some("doc") | Some("ppt") | Some("xls") => {
-                scan_legacy_office(&path, &patterns).unwrap_or(None)
-            }
+            Some("rtf") | Some("wpd") => scan_rtf(&path, &patterns),
+            Some("doc") | Some("ppt") | Some("xls") => scan_legacy_office(&path, &patterns),
             Some("msg") => scan_msg(&path, &patterns),
             _ => None,
         },
@@ -33,32 +28,32 @@ pub fn scan_file(path: &PathBuf, patterns: &Vec<Regex>) -> Option<(Vec<String>, 
 }
 
 fn scan_msg(path: &PathBuf, patterns: &Vec<Regex>) -> Option<(Vec<String>, String)> {
-    let mut findings: Vec<String> = Vec::new();
     let content = match Outlook::from_path(path) {
         Ok(c) => c,
-        Err(_) => return None,
-    };
-    let content = content.to_json();
-    for pattern in patterns {
-        match pattern.captures(content.as_ref().unwrap()) {
-            Some(cap) => {
-                for finding in cap.iter() {
-                    let finding = finding.unwrap().as_str().to_string();
-                    if !findings.contains(&finding) {
-                        findings.push(finding);
-                    }
-                }
-            }
-            _ => (),
+        Err(e) => {
+            log::error!("failed to load msg file: {}", e);
+            return None;
         }
+    };
+    let content = match content.to_json() {
+        Ok(c) => c,
+        Err(e) => {
+            log::error!("failed to load msg json: {}", e);
+            return None;
+        }
+    };
+
+    let findings = search_content(content, patterns);
+
+    if findings.len() > 0 {
+        return Some((findings, path.to_str().unwrap().to_string()));
     }
-    Some((findings, path.to_str().unwrap().to_string()))
+
+    None
 }
 
 fn scan_pdf(path: &PathBuf, patterns: &Vec<Regex>) -> Option<(Vec<String>, String)> {
     use lopdf::Document;
-
-    let mut findings = Vec::new();
 
     let content = match Document::load(path) {
         Ok(doc) => {
@@ -73,81 +68,74 @@ fn scan_pdf(path: &PathBuf, patterns: &Vec<Regex>) -> Option<(Vec<String>, Strin
 
             texts
         }
-        Err(_) => return None,
+        Err(e) => {
+            log::error!("failed to load pdf: {}", e);
+            return None;
+        }
     };
 
-    for pattern in patterns.iter() {
-        match pattern.captures(&content) {
-            Some(cap) => {
-                for finding in cap.iter() {
-                    let finding = finding.unwrap().as_str().to_string();
-                    if !findings.contains(&finding) {
-                        findings.push(finding);
-                    }
-                }
-            }
-            _ => (),
-        }
+    let findings = search_content(content, patterns);
+
+    if findings.len() > 0 {
+        return Some((findings, path.to_str().unwrap().to_string()));
     }
 
-    Some((findings, path.to_str().unwrap().to_string()))
+    None
 }
 
-fn scan_ooxml(
-    path: &PathBuf,
-    patterns: &Vec<Regex>,
-) -> Result<Option<(Vec<String>, String)>, Box<dyn Error>> {
-    let mut findings = vec![];
+fn scan_ooxml(path: &PathBuf, patterns: &Vec<Regex>) -> Option<(Vec<String>, String)> {
     let file = fs::File::open(path).unwrap();
     let mut archive = match zip::ZipArchive::new(file) {
         Ok(z) => z,
-        Err(_) => return Ok(None),
+        Err(_) => return None,
     };
     let mut items = vec![];
-
+    let mut content = String::new();
     for item in archive.file_names() {
         items.push(item.to_owned());
     }
     for item in items {
-        let reader = BufReader::new(archive.by_name(&item)?);
+        let reader = BufReader::new(match archive.by_name(&item) {
+            Ok(i) => i,
+            Err(_) => continue,
+        });
         let parser = EventReader::new(reader);
         for event in parser {
             match event {
                 Ok(xml::reader::XmlEvent::Characters(c)) => {
-                    for pattern in patterns.iter() {
-                        match pattern.captures(&c) {
-                            Some(cap) => {
-                                for finding in cap.iter() {
-                                    let finding = finding.unwrap().as_str().to_string();
-                                    if !findings.contains(&finding) {
-                                        findings.push(finding);
-                                    }
-                                }
-                            }
-                            _ => (),
-                        }
-                    }
+                    content.push_str(&c);
                 }
                 _ => continue,
             }
         }
     }
 
+    let findings = search_content(content, patterns);
+
     if findings.len() > 0 {
-        return Ok(Some((findings, path.to_str().unwrap().to_string())));
+        return Some((findings, path.to_str().unwrap().to_string()));
     }
 
-    Ok(None)
+    None
 }
 
-fn scan_legacy_office(
-    path: &PathBuf,
-    patterns: &Vec<Regex>,
-) -> Result<Option<(Vec<String>, String)>, Box<dyn Error>> {
-    let mut findings = vec![];
-    let mut file = fs::File::open(path)?;
+fn scan_legacy_office(path: &PathBuf, patterns: &Vec<Regex>) -> Option<(Vec<String>, String)> {
+    // let mut findings = vec![];
+    let mut file = match fs::File::open(path) {
+        Ok(f) => f,
+        Err(e) => {
+            log::error!("failed to open legacy office file: {}", e);
+            return None;
+        }
+    };
     let mut buffer = vec![];
-    file.read_to_end(&mut buffer)?;
+    match file.read_to_end(&mut buffer) {
+        Ok(_) => (),
+        Err(e) => {
+            log::error!("legacy office file failed to read: {}", e);
+            return None;
+        }
+    };
     let mut content = String::new();
     for c in &buffer {
         if c.is_ascii() {
@@ -161,36 +149,105 @@ fn scan_legacy_office(
         }
     }
 
-    for pattern in patterns.iter() {
-        match pattern.captures(&content) {
-            Some(cap) => {
-                for finding in cap.iter() {
-                    let finding = finding.unwrap().as_str().to_string();
-                    if !findings.contains(&finding) {
-                        findings.push(finding);
-                    }
-                }
-            }
-            _ => (),
-        }
-    }
+    // for pattern in patterns.iter() {
+    //     match pattern.captures(&content) {
+    //         Some(cap) => {
+    //             for finding in cap.iter() {
+    //                 let finding = finding.unwrap().as_str().to_string();
+    //                 if !findings.contains(&finding) {
+    //                     findings.push(finding);
+    //                 }
+    //             }
+    //         }
+    //         _ => (),
+    //     }
+    // }
+
+    let findings = search_content(content, patterns);
 
     if findings.len() > 0 {
-        return Ok(Some((findings, path.to_str().unwrap().to_string())));
+        return Some((findings, path.to_str().unwrap().to_string()));
     }
 
-    Ok(None)
+    None
 }
 
-fn scan_txt(
-    path: &PathBuf,
-    patterns: &Vec<Regex>,
-) -> Result<Option<(Vec<String>, String)>, Box<dyn Error>> {
-    let mut findings: Vec<String> = vec![];
+fn scan_txt(path: &PathBuf, patterns: &Vec<Regex>) -> Option<(Vec<String>, String)> {
+    // let mut findings: Vec<String> = vec![];
     let content = match fs::read_to_string(path) {
         Ok(c) => c,
-        Err(_) => return Ok(None), //TODO log
+        Err(e) => {
+            log::error!("ERROR reading txt file: {}", e);
+            return None;
+        }
     };
+    // for pattern in patterns.iter() {
+    //     match pattern.captures(&content) {
+    //         Some(cap) => {
+    //             for finding in cap.iter() {
+    //                 let finding = finding.unwrap().as_str().to_string();
+    //                 if !findings.contains(&finding) {
+    //                     findings.push(finding);
+    //                 }
+    //             }
+    //         }
+    //         _ => (),
+    //     }
+    // }
+
+    let findings = search_content(content, patterns);
+
+    if findings.len() > 0 {
+        return Some((findings, path.to_str().unwrap().to_string()));
+    }
+
+    None
+}
+
+fn scan_rtf(path: &PathBuf, patterns: &Vec<Regex>) -> Option<(Vec<String>, String)> {
+    // let mut findings = vec![];
+    let file = match fs::File::open(path) {
+        Ok(f) => f,
+        Err(e) => {
+            log::error!("ERROR rtf failed to load: {}", e);
+            return None;
+        }
+    };
+    let mut reader = BufReader::new(file);
+    let mut content = String::new();
+    match reader.read_to_string(&mut content) {
+        Ok(_) => (),
+        Err(e) => {
+            log::error!("ERROR processing rtf: {}", e);
+        }
+    }
+
+    // for pattern in patterns.iter() {
+    //     match pattern.captures(&buf) {
+    //         Some(cap) => {
+    //             for finding in cap.iter() {
+    //                 let finding = finding.unwrap().as_str().to_string();
+    //                 if !findings.contains(&finding) {
+    //                     findings.push(finding);
+    //                 }
+    //             }
+    //         }
+    //         _ => (),
+    //     }
+    // }
+
+    let findings = search_content(content, patterns);
+
+    if findings.len() > 0 {
+        return Some((findings, path.to_str().unwrap().to_string()));
+    }
+
+    None
+}
+
+fn search_content(content: String, patterns: &Vec<Regex>) -> Vec<String> {
+    let mut findings = Vec::new();
+
     for pattern in patterns.iter() {
         match pattern.captures(&content) {
             Some(cap) => {
@@ -201,42 +258,9 @@ fn scan_txt(
                     }
                 }
             }
-            _ => (),
-        }
-    }
-    if findings.len() > 0 {
-        return Ok(Some((findings, path.to_str().unwrap().to_string())));
-    }
-    Ok(None)
-}
-
-fn scan_rtf(
-    path: &PathBuf,
-    patterns: &Vec<Regex>,
-) -> Result<Option<(Vec<String>, String)>, Box<dyn Error>> {
-    let mut findings = vec![];
-    let file = fs::File::open(path)?;
-    let mut reader = BufReader::new(file);
-    let mut buf = String::new();
-    reader.read_to_string(&mut buf)?;
-
-    for pattern in patterns.iter() {
-        match pattern.captures(&buf) {
-            Some(cap) => {
-                for finding in cap.iter() {
-                    let finding = finding.unwrap().as_str().to_string();
-                    if !findings.contains(&finding) {
-                        findings.push(finding);
-                    }
-                }
-            }
-            _ => (),
+            None => (),
         }
     }
 
-    if findings.len() > 0 {
-        return Ok(Some((findings, path.to_str().unwrap().to_string())));
-    }
-
-    Ok(None)
+    findings
 }
