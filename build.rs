6cf7e817bd50d24e8518fa9142f904cd9399eb44
Bryan Vinton <bryan.vinton18@gmail.com>
2023-07-28 15:07:16 -0400

    clean up

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ extern crate winresource;
 
 fn main() {
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
-        let mut res = winresource::WindowsResource::new();
+        let res = winresource::WindowsResource::new();
         res.compile().unwrap();
     }
 }
